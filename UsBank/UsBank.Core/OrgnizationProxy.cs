4e68818d94f83ee8c59c2ee49e955797e26bb6fd
sushant3239 <sushant_patil@outlook.com>
2014-10-20 19:45:41 +0530

    Created HubTile

---
+++ UsBank/UsBank.Core/OrgnizationProxy.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Net;
+using System.ServiceModel.Description;
+using System.Text;
+using UsBank.Core.ServiceReference1;
+
+namespace UsBank.Core
+{
+    public class OrgnizationProxy
+    {
+        public OrgnizationProxy()
+        {
+            //IOrganizationService orgnizationService = GetOrganizationService();
+            //StringBuilder sb = new StringBuilder();
+            //QueryExpression query = new QueryExpression("phonecall");
+            //query.ColumnSet = new ColumnSet("description", "modifiedby");
+            //EntityCollection result = orgnizationService.RetrieveMultiple(query);
+
+            OrganizationServiceClient client = new OrganizationServiceClient(new System.ServiceModel.BasicHttpBinding(), 
+                new System.ServiceModel.EndpointAddress("https://cts11384.api.crm5.dynamics.com/XRMServices/2011/Organization.svc"));
+        }
+
+        internal static OrganizationServiceProxy GetOrganizationService()
+        {
+            try
+            {
+                ClientCredentials Credentials = new ClientCredentials();
+                Credentials.Windows.ClientCredential = CredentialCache.DefaultNetworkCredentials;
+
+                //// Set the crmDomain
+                Credentials.Windows.ClientCredential.Domain = "CTS";
+
+                //// Set the crmUser
+                Credentials.UserName.UserName = "pankajsvnit@cts11384.onmicrosoft.com";
+                //Credentials.UserName.UserName = "bansal";
+
+                //// Set the crmPassword
+                Credentials.UserName.Password = "236eL4in";
+                //Credentials.UserName.Password = "B15nCrDe";
+
+                //Uri OrganizationUri = new Uri("http://bisw08tcrm0001/OneCRM/XRMServices/2011/Organization.svc");
+                Uri OrganizationUri = new Uri("https://cts11384.api.crm5.dynamics.com/XRMServices/2011/Organization.svc");
+                Uri HomeRealmUri = null;
+
+                OrganizationServiceProxy serviceProxy = new OrganizationServiceProxy(OrganizationUri, HomeRealmUri, Credentials, null);
+                serviceProxy.EnableProxyTypes();
+
+                return serviceProxy;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ UsBank/UsBank/Controls/HubTile.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace UsBank.Controls
+{
+    public sealed partial class HubTile : UserControl
+    {
+        private bool isActive = false;
+
+        public HubTile()
+        {
+            this.InitializeComponent();
+        }
+
+        public string HeaderText
+        {
+            get { return (string)GetValue(HeaderTextProperty); }
+            set { SetValue(HeaderTextProperty, value); }
+        }
+
+        public static readonly DependencyProperty HeaderTextProperty =
+            DependencyProperty.Register("HeaderText", typeof(string), typeof(HubTile), new PropertyMetadata(String.Empty));
+
+
+        public ImageSource ActiveTileImage
+        {
+            get { return (ImageSource)GetValue(ActiveTileImageProperty); }
+            set { SetValue(ActiveTileImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty ActiveTileImageProperty =
+            DependencyProperty.Register("ActiveTileImage", typeof(ImageSource), typeof(HubTile), null);
+
+
+        public ImageSource InActiveTileImage
+        {
+            get { return (ImageSource)GetValue(InActiveTileImageProperty); }
+            set { SetValue(InActiveTileImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty InActiveTileImageProperty =
+            DependencyProperty.Register("InActiveTileImage", typeof(ImageSource), typeof(HubTile), null);
+
+        private void StackPanelTapped(object sender, TappedRoutedEventArgs e)
+        {
+            var panel = sender as StackPanel;
+            if (panel != null)
+            {
+                SwitchTileStatus();
+            }
+        }
+
+        private void SwitchTileStatus()
+        {
+            if (isActive)
+            {
+                
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
