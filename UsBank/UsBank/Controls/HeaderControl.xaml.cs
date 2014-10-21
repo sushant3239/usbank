0799eec6a390addb9f2f92e8e5a93ae0ea263812
sushant3239 <sushant_patil@outlook.com>
2014-10-21 19:28:20 +0530

    Added bineries folder

--- UsBank/UsBank/Controls/HeaderControl.xaml.cs
+++ UsBank/UsBank/Controls/HeaderControl.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using UsBank.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -11,6 +12,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
@@ -22,6 +24,14 @@ namespace UsBank.Controls
         public HeaderControl()
         {
             this.InitializeComponent();
+            Loaded += (sender, e) =>
+            {
+                var typeResolver = Container.TypeResolver.Instance;
+                var accountManager = typeResolver.Resolve<IAccountManager>();
+                var user = accountManager.CurrentUser;
+                UserName = user.UserId;
+                UserRole = "Region Manager";
+            };
         }
 
 
