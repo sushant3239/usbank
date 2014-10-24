b7fb09775e887ba5e60304882d107e7719227d17
sushant3239 <sushant_patil@outlook.com>
2014-10-24 15:38:31 +0530

    changes some UI

---
+++ UsBank/UsBank/Converters/DateFormatConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace UsBank.Converters
+{
+    public class DateFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            DateTime date = DateTime.Parse(value as string);            
+            return date.ToString("dd/mm/yyyy");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
